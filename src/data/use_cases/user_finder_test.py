6e1f4c5bbb36bcf7b8f0b0a7341e8774b5a17873
Michael Pablo Gomes D Silva <michael_pablo13@hotmail.com>
2024-02-26 19:53:54 -0300

    feat: Implementing user_register

--- src/data/use_cases/user_finder_test.py
+++ src/data/use_cases/user_finder_test.py
@@ -1,6 +1,61 @@
-from src.infra.db.repositories.users_repository import UsersRepository
+from src.infra.db.tests.users_repository import UsersRepositorySpy
 from .user_finder import UserFinder
 
+
 def test_find():
-    repo = UsersRepository()
+    first_name = "meunome"
+
+    repo = UsersRepositorySpy()
     user_finder = UserFinder(repo)
+
+    response = user_finder.find(first_name)
+
+    assert repo.select_user_attributes["first_name"] == first_name
+
+    assert response["type"] == "Users"
+    assert response["count"] == len(response["attributes"])
+    assert response["attributes"] != []
+
+
+def test_find_error_in_valid_name():
+    first_name = "meunome123"
+
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as expection:
+        assert str(expection) == "Nome invalido para a busca"
+
+
+def test_find_error_in_long_name():
+    first_name = "meunomeASDSADsadasffffasadsafasdasfasd"
+
+    repo = UsersRepositorySpy()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as expection:
+        assert str(expection) == "Nome muito grande para busca"
+
+
+def test_find_error_user_not_found():
+
+    class UsersRepositoryError(UsersRepositorySpy):
+        def select_user(self, first_name: str):
+            return []
+
+    first_name = "meunome"
+
+    repo = UsersRepositoryError()
+    user_finder = UserFinder(repo)
+
+    try:
+        user_finder.find(first_name)
+        assert False
+    except Exception as expection:
+        assert str(expection) == "Usuario nao encontrado"
